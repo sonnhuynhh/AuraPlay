58dd7e2701cb825abac5b6b5d47bc362226c19e1
sonnhuynhh <hbts2303@gmail.com>
2026-04-07 17:28:11 +0700

    first commit

---
+++ src/main/java/com/sonnhuynhh/auraplay/AuraPlayApplication.java
@@ -0,0 +1,13 @@
+package com.sonnhuynhh.auraplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonnhuynhh/auraplay/AuraPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonnhuynhh.auraplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
